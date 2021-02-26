da3e6b7df5833e0770ecc00bbb12af50797b8108
Alessia <alessia@mipsscan.com>
2021-02-26 11:28:24 -0500

    Added Small Scene

---
+++ Studio2/Assets/SceneChanger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    public string sceneName;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Feet")
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
