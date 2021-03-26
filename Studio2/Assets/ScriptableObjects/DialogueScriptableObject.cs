f8fcccc424d13725650c361ae4097a4f4addd94f
Alessia <alessia@mipsscan.com>
2021-03-26 01:50:43 -0400

    Added Scriptable Object

---
+++ Studio2/Assets/ScriptableObjects/DialogueScriptableObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "Dialogue Block", 
+    menuName = "ScriptableObjects/Dialogue Block",
+    order = 0)]
+
+public class DialogueScriptableObject : ScriptableObject
+{
+    public string header;
+    public string character;
+    public string dialogue;
+    public Sprite characterPortrait;
+    public DialogueScriptableObject nextLine;
+    //If this is the end block, the 'next convo' line will be used to begin the next conversation
+    public bool isEnd;
+    public DialogueScriptableObject nextConvo;
+    //This is for if the player has the option to choose a piece of dialogue
+    public bool hasReply;
+    public string[] replies;
+    public DialogueScriptableObject[] replyAnswers;
+    //This is for if you can trigger alternative dialogue by holding an item
+    public bool hasEvent; 
+    public GameObject eventItem;
+    public DialogueScriptableObject alternativeBlock;
+}
