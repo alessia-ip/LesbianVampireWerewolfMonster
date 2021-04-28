9dd6b166ad0ee6c4162c9b15d0e8cfe772a587fd
UpbeatBeau <70418283+UpbeatBeau@users.noreply.github.com>
2021-04-28 01:07:12 -0400

    Outline with hover

---
+++ Studio2/Assets/Scripts/beauScripts/OutlineHover.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineHover : MonoBehaviour
+{
+   public Material outline;
+
+
+   private void Start()
+   {
+       
+   }
+
+   private void OnMouseOver()
+   {
+    outline.SetFloat("_Thickness", 2.5f);
+       
+   }
+
+   private void OnMouseExit()
+   {
+       outline.SetFloat("_Thickness", 0f);
+   }
+}
