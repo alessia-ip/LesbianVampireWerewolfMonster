f6769e20e2d3f33de06d87c583b1e12a0d970d17
Alessia <alessia@mipsscan.com>
2021-04-30 12:21:58 -0400

    Sequence Break Fixed

---
+++ Studio2/Assets/OneOffEvents.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OneOffEvents : MonoBehaviour
+{
+
+    public TextMeshProUGUI dialogueCheck;
+
+    public DialogueScriptableObject mapGetDialogue;
+    public GameObject mapButton;
+    public GameObject mapCollider;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dialogueCheck.text == mapGetDialogue.altDialogue)
+        {
+            mapButton.SetActive(true);
+            mapCollider.SetActive(true);
+        }
+    }
+}
