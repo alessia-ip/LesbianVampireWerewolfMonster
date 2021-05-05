2597fc79a5342475a122615ef306a53244419337
jude <kap9566@nyu.edu>
2021-05-05 10:00:25 -0400

    last push 4 noww

---
+++ Studio2/Assets/PlayAudioOnEnter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioOnEnter : MonoBehaviour
+{
+    public AudioSource music;
+    
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player" && !music.isPlaying) {
+            music.Play();
+        }
+    }
+    void OnTriggerExit2D(Collider2D col) { 
+     if (col.tag == "Player") {
+            music.Stop();
+        }
+    }
+}
